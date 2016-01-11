86c96016ef19eea596110147a89e1af8adb62452
knowledgecode <knowledgecode@gmail.com>
2016-01-11 19:43:32 +0900

    add tests for multi language

---
+++ test/ar.js
@@ -0,0 +1,84 @@
+(function (global) {
+    'use strict';
+
+    var expect = global.expect || require('expect.js'),
+        date = global.date || require('../date-and-time'),
+        forEach = function (array, fn) {
+            for (var i = 0, len = array.length; i < len; i++) {
+                if (fn(array[i], i) === 0) {
+                    break;
+                }
+            }
+        },
+        MMMM = ['كانون الثاني يناير', 'شباط فبراير', 'آذار مارس', 'نيسان أبريل', 'أيار مايو', 'حزيران يونيو', 'تموز يوليو', 'آب أغسطس', 'أيلول سبتمبر', 'تشرين الأول أكتوبر', 'تشرين الثاني نوفمبر', 'كانون الأول ديسمبر'],
+        MMM = ['كانون الثاني يناير', 'شباط فبراير', 'آذار مارس', 'نيسان أبريل', 'أيار مايو', 'حزيران يونيو', 'تموز يوليو', 'آب أغسطس', 'أيلول سبتمبر', 'تشرين الأول أكتوبر', 'تشرين الثاني نوفمبر', 'كانون الأول ديسمبر'],
+        dddd = ['الأحد', 'الإثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت'],
+        ddd = ['أحد', 'إثنين', 'ثلاثاء', 'أربعاء', 'خميس', 'جمعة', 'سبت'],
+        dd = ['ح', 'ن', 'ث', 'ر', 'خ', 'ج', 'س'],
+        A = ['ص', 'ص', 'ص', 'ص', 'ص', 'ص', 'ص', 'ص', 'ص', 'ص', 'ص', 'ص',    // 00 - 11
+             'م', 'م', 'م', 'م', 'م', 'م', 'م', 'م', 'م', 'م', 'م', 'م'];   // 12 - 23
+
+    date.locale('ar');
+
+    describe('format', function () {
+        forEach(MMMM, function (m, i) {
+            it('"MMMM" equals to "' + m + '"', function () {
+                var now = new Date(2015, i, 1, 12, 34, 56, 789);
+                expect(date.format(now, 'MMMM')).to.equal(m);
+            });
+        });
+        forEach(MMM, function (m, i) {
+            it('"MMM" equals to "' + m + '"', function () {
+                var now = new Date(2015, i, 1, 12, 34, 56, 789);
+                expect(date.format(now, 'MMM')).to.equal(m);
+            });
+        });
+        forEach(dddd, function (d, i) {
+            it('"dddd" equals to "' + d + '"', function () {
+                var now = new Date(2016, 0, i + 3, 12, 34, 56, 789);
+                expect(date.format(now, 'dddd')).to.equal(d);
+            });
+        });
+        forEach(ddd, function (d, i) {
+            it('"ddd" equals to "' + d + '"', function () {
+                var now = new Date(2016, 0, i + 3, 12, 34, 56, 789);
+                expect(date.format(now, 'ddd')).to.equal(d);
+            });
+        });
+        forEach(dd, function (d, i) {
+            it('"dd" equals to "' + d + '"', function () {
+                var now = new Date(2016, 0, i + 3, 12, 34, 56, 789);
+                expect(date.format(now, 'dd')).to.equal(d);
+            });
+        });
+        forEach(A, function (a, i) {
+            it('"A" equals to "' + a + '"', function () {
+                var now = new Date(2016, 0, 1, i, 34, 56, 789);
+                expect(date.format(now, 'A')).to.equal(a);
+            });
+        });
+    });
+
+    describe('parse', function () {
+        forEach(MMMM, function (m, i) {
+            it('"MMMM"', function () {
+                var now = new Date(0, i, 1);
+                expect(date.parse(m, 'MMMM')).to.eql(now);
+            });
+        });
+        forEach(MMM, function (m, i) {
+            it('"MMM"', function () {
+                var now = new Date(0, i, 1);
+                expect(date.parse(m, 'MMM')).to.eql(now);
+            });
+        });
+        forEach(A, function (a, i) {
+            it('h A', function () {
+                var now = new Date(0, 0, 1, i);
+                expect(date.parse((i > 11 ? i - 12 : i) + ' ' + a, 'h A')).to.eql(now);
+            });
+        });
+    });
+
+}(this));
+
