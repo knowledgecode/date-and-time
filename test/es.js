0d98dc998e81a9c733a3b4847f099fb8efd7a40d
angelOrtiz1 <44440168+angelOrtiz1@users.noreply.github.com>
2018-10-24 15:03:24 -0500

    Update es.js
    updated test

--- test/es.js
+++ test/es.js
@@ -10,14 +10,14 @@
                 }
             }
         },
-        MMMM = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
-        MMM = ['ene.', 'feb.', 'mar.', 'abr.', 'may.', 'jun.', 'jul.', 'ago.', 'sep.', 'oct.', 'nov.', 'dic.'],
-        dddd = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'],
-        ddd = ['dom.', 'lun.', 'mar.', 'mié.', 'jue.', 'vie.', 'sáb.'],
-        dd = ['do', 'lu', 'ma', 'mi', 'ju', 'vi', 'sá'],
+        MMMM = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        MMM = ['Ene.', 'Feb.', 'Mar.', 'Abr.', 'May.', 'Jun.', 'Jul.', 'Ago.', 'Sep.', 'Oct.', 'Nov.', 'Dic.'],
+        dddd = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+        ddd = ['Dom.', 'Lun.', 'Mar.', 'Mié.', 'Jue.', 'Vie.', 'Sáb.'],
+        dd = ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
         A = ['de la mañana', 'de la mañana', 'de la mañana', 'de la mañana', 'de la mañana', 'de la mañana', 'de la mañana', 'de la mañana', 'de la mañana', 'de la mañana', 'de la mañana', 'de la mañana',    // 0 - 11
             'de la tarde', 'de la tarde', 'de la tarde', 'de la tarde', 'de la tarde', 'de la tarde', 'de la tarde',    // 12 - 18
-            'de la noche', 'de la noche', 'de la noche', 'de la noche', 'de la noche'];     // 19 - 23
+            'de la noche', 'de la noche', 'de la noche', 'de la noche', 'de la noche'];      // 19 - 23
 
     describe('format with "es"', function () {
         before(function () {
