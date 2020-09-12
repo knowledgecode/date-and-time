5e13caae356fe1b7cfdf2daf2e6e1527386b8ea4
Yaron Shani <yaron.shani@gmail.com>
2020-09-12 15:31:53 +0300

    add hebrew

---
+++ locale/he.js
@@ -0,0 +1,35 @@
+/**
+ * @preserve date-and-time.js locale configuration
+ * @preserve Hebrew (HE)
+ * @preserve It is using moment.js locale configuration as a reference.
+ */
+(function (global) {
+    'use strict';
+
+    var exec = function (date) {
+        var code = 'he';
+
+        date.locale('he', {
+            res: {
+                MMMM: ['ינואר', 'פברואר', 'מרץ', 'אפריל', 'מאי', 'יוני', 'יולי', 'אוגוסט', 'ספטמבר', 'אוקטובר', 'נובמבר', 'דצמבר'],
+                MMM: ['ינו׳', 'פבר׳', 'מרץ', 'אפר׳', 'מאי', 'יוני', 'יולי', 'אוג׳', 'ספט׳', 'אוק׳', 'נוב׳', 'דצמ׳],
+                dddd: [	'יום ראשון', 'יום שני', 'יום שלישי', 'יום רביעי', 'יום חמישי', 'יום שישי', 'יום שבת'],
+                ddd: ['יום א׳', 'יום ב׳', 'יום ג׳', 'יום ד׳', 'יום ה׳', 'יום ו׳', 'שבת'],
+                dd: ['א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ש']
+            }
+        });
+        return code;
+    };
+
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        (module.paths || []).push('./');
+        module.exports = exec;
+        // This line will be removed in the next version.
+        exec(require('date-and-time'));
+    } else if (typeof define === 'function' && define.amd) {
+        define(['date-and-time'], exec);
+    } else {
+        exec(global.date);
+    }
+
+}(this));
