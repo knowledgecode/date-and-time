cadc9b696ea67b185709a82293f49013d85c4b6f
knowledgecode <knowledgecode@gmail.com>
2022-03-09 21:54:40 +0900

    Fixed an issue where parseTZ() could return NaN instead of Invalid Date if parsing failed.

--- src/plugin/timezone.js
+++ src/plugin/timezone.js
@@ -54,7 +54,7 @@ var plugin = function (date, localized_date) {
                 return d;
             }
         }
-        return NaN;
+        return new Date(NaN);
     };
     var transformTZ = function (dateString, arg1, arg2, timeZone) {
         return formatTZ(localized_date.parse(dateString, arg1), arg2, timeZone);
